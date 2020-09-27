d357bc4cfdf91859bf6f5f184d3921305df87116
maxioul <maxiou7391468250@126.com>
2020-09-27 12:16:43 +0900

    Site updated: 2020-09-27 12:16:42

---
+++ open_diary.bat
@@ -0,0 +1,10 @@
+cd C:\Users\MR.MA\maxioul.github.io\bolg
+set yyyy=%date:~,4%
+set mm=%date:~5,2%
+set day=%date:~8,2% 
+set filename=%yyyy%-%mm%-%day%
+set filename=%filename: =0%
+set filename=%filename:~,10%
+set filename=C:\Users\MR.MA\maxioul.github.io\bolg\source\_posts\%filename%.md
+start %filename%
+exit
